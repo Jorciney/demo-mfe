23e7affef3b01c4bf76cdbdc9aea1a661dd8a216
Jorciney <jorciney.dias@hotmail.com>
2024-08-30 16:54:34 +0200

    testing native federation with @ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TitleComponent } from '@libs';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, TitleComponent],
+  imports: [RouterModule, TitleComponent, TranslateModule],
+  providers: [TranslateService],
   selector: 'app-root',
-  template: `Test fom MFE <app-title></app-title>`,
-  styleUrl: './app.component.scss',
+  template: `
+    <div style="background-color: yellow"> This is the MFE
+      <app-title></app-title>
+    </div>`,
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title = 'demo-mfe';
+  translateService = inject(TranslateService)
+
+  ngOnInit() {
+    console.log('AppComponent ', this.translateService.getLangs());
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot())
+  ]
 };
